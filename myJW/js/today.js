8e8c5ec019b4c3c7f3127c78a80f749327b030e1
Michalik, Joshua <jsmichal@usa.wtbts.net>
2015-11-04 19:36:46 +0000

    Add done button on today screen

--- myJW/js/today.js
+++ myJW/js/today.js
@@ -1,8 +1,9 @@
 
-$(function() {
+function init() {
    $('#todayDate').html(getTodaysDate());
-   getNextReading();;
-});
+   getNextReading();
+}
+init();
 
 function getTodaysDate() {
    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -39,22 +40,52 @@ function formatReading(aReading) {
          strReading += " " + parseInt(chapID);
       }
       $('#reading').html(strReading);
+      $('#done').click(function(e) {
+         for (var i = 0; i < aReading.Reading.length; i++) {
+            markAsComplete(aReading.Reading[i]);
+            getNextReading();
+         }
+      });
    })
 }
 
 function getNextReading() {
-   return $.getJSON('js/json/schedule01.json', function(d) {
+   $.getJSON('js/json/schedule01.json', function(d) {
       var progress = JSON.parse(localStorage.getItem('progress'));
       var completedChapters = (progress) ? progress.CompletedChapters : [];
       for (var i = 0; i < d.Schedule.length; i++) {
          for (var j = 0; j < d.Schedule[i].Reading.length; j++) {
             var read = d.Schedule[i].Reading[j];
             if ($.inArray(read, completedChapters) === -1) {
-               // console.log(d.Schedule[i]);
                formatReading(d.Schedule[i]);
                return;
             }
          }
       }
    });
+}
+
+function markAsComplete(intChapID) {
+      var savedProgress = JSON.parse(localStorage.getItem('progress'));
+      if (!savedProgress) {
+         savedProgress = initSaveProgress();
+      }
+      if (!isChapterComplete(intChapID)) {
+         savedProgress.CompletedChapters.push(intChapID);
+         localStorage.setItem('progress', JSON.stringify(savedProgress));
+      }
+}
+
+function initSaveProgress() {
+   localStorage.clear();
+   var progress = {};
+   progress.StartDate = "November 3, 2015";
+   progress.CompletedChapters = [];
+   return progress;
+}
+
+function isChapterComplete(intChapID) {
+   var savedProgress = JSON.parse(localStorage.getItem('progress'));
+   var completeChapters = (savedProgress) ? savedProgress.CompletedChapters : [];
+   return ($.inArray(intChapID, completeChapters) !== -1);
 }
