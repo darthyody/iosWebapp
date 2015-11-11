b6836448c8b7cb1f21b2cfc5ead3ba29624c796e
Michalik, Joshua <jsmichal@usa.wtbts.net>
2015-11-11 18:21:54 +0000

    add temp console for debugging

--- myJW/js/settings.js
+++ myJW/js/settings.js
@@ -7,11 +7,29 @@ function init() {
    var blOk = (Progress.CompletedChapters.length === 0) ? true : false;
    $('#autocomplete').prop('checked', blOk);
    $('#nocomplete').prop('checked', !blOk);
+
+
+
+
+   var $csl = $('#console');
+   var dateNow    = new Date();
+   var dateNowFm  = DateTool.getFormattedDate(new Date());
+   var dateCtd    = DateTool.getFormattedDate(new Date('2015 11 11'));
+   var savedNow   = Progress.StartDate;
+   var savedNowFm = DateTool.getFormattedDate(new Date(Progress.StartDate));
+   $csl.append("<p>NOW: " + typeof dateNow + dateNow + "</p>");
+   $csl.append("<p>NOW FRMT: " + typeof dateNowFm + dateNowFm + "</p>");
+   $csl.append("<p>NOW CREATED: " + typeof dateCtd + dateCtd + "</p>");
+   $csl.append("<p>SAVED: " + typeof + savedNow + savedNow + "</p>");
+   $csl.append("<p>SAVED FRMT: " + typeof + savedNowFm + savedNowFm + "</p>");
 }
 init();
 
 $('#save').click(function() {
+   var $csl    = $('#console');
    var date = DateTool.getFormattedDate(new Date($('#startdate').val()));
+   $csl.append("<p>pre-reset: " + typeof + Progress.StartDate + Progress.StartDate + "</p>");
+
    var blOk = ($('#autocomplete:checked').val()) ? true : false;
    Progress.StartDate = date;
    if (blOk) {
@@ -20,18 +38,26 @@ $('#save').click(function() {
          for (var j = 0; j < Schedule.Schedule[i].Reading.length; j++) {
             var intChapID = Schedule.Schedule[i].Reading[j];
             Progress.addChapter(intChapID);
-            Progress.save();
          }
       }
    }
    Progress.save();
-   location.reload();
+   // location.reload();
+
+   var newDate = $('#startdate').val();
+   $csl.append("<p>NEW DATE: " + typeof + newDate + newDate + "</p>");
+   $csl.append("<p>SAVE AS: " + typeof + date + date + "</p>");
+   $csl.append("<p>post-reset: " + typeof + Progress.StartDate + Progress.StartDate + "</p>");
 });
 
 $('#reset').click(function() {
+   var $csl    = $('#console');
+   $csl.append("<p>pre-reset: " + typeof + Progress.StartDate + Progress.StartDate + "</p>");
    var yesNo = confirm("Are you sure you want to reset your schedule?");
    if(yesNo) {
       Progress.initSavedProgress();
-      location.reload();
+      // location.reload();
    }
+
+   $csl.append("<p>post-reset: " + typeof + Progress.StartDate + Progress.StartDate + "</p>");
 });
